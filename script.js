face0401e4473a461ab4610ace68e087f88df55f
sherman254 <hillarynyende03@gmail.com>
2025-09-08 15:22:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Portfolio Script
+console.log("Portfolio Loaded!");
+
+// Example: Smooth scrolling for navbar links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
